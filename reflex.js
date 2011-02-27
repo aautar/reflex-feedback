19c5b4983565b718c9c728f7515024f53be8f923
Avishkar Autar <aautar@gmail.com>
2011-02-27 15:01:11 -0500

    v0.2 new layout, jquery-ui buttons menu, new icons, css fixes (prevent conflicting inherited styles)

--- reflex.js
+++ reflex.js
@@ -1,9 +1,9 @@
 /*
     
-Reflex Feedback
+Reflex Feedback v0.2
 AJAX, JQuery UI widget for user feedback
 
-Copyright (c) 2010, Avishkar Autar
+Copyright (c) 2010-2011, Avishkar Autar
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -31,10 +31,12 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 var Reflex = {}
 
+Reflex.version = 0.2;
 Reflex.feedbackType = 'idea';
 Reflex.txtAreaHasDefault = true;
 Reflex.curDefaultTxt = '';
 Reflex.feedbackPostUrl = 'test.php';
+Reflex.ghostTxtColor = '#999';
 
 Reflex.sendFeedback = function (feedbackType, feedbackTxt)
 {
@@ -61,6 +63,7 @@ Reflex.sendFeedback = function (feedbackType, feedbackTxt)
 
 Reflex.onSendSuccess = function ()
 {
+    $('#reflex-feedback-dialog').dialog('enable');
     $('#reflex-feedback-dialog').dialog('close');
     $('#reflex-feedback-send-success-dialog').dialog({ title: 'Thank you!' }, { width: 400 }, { modal: true }, { resizable: false }, { draggable: true }, 
     { buttons: { "OK": function ()
@@ -73,49 +76,30 @@ Reflex.onSendSuccess = function ()
 
 Reflex.onSendFailure = function ()
 {
+    $('#reflex-feedback-dialog').dialog('enable');
     $('#reflex-feedback-send-failure-msg').show();
+    $('#reflex-feedback-dialog-entry-area-submitsec .reflex-feedback-sending-msg').hide();
 
-    $('#reflex-feedback-dialog-entry-area-submitsec .reflex-feedback-preloader').hide();
-
-    $("#reflex-feedback-dialog-entry-area button").button('refresh');
-    $("#reflex-feedback-dialog-entry-area button").button('enable');
-    Reflex.bindSendButton();
-}
-
-Reflex.bindSendButton = function ()
-{
-    $("#reflex-feedback-dialog-entry-area button").click(function ()
-    {
-        $('#reflex-feedback-send-failure-msg').hide();
-        $('#reflex-feedback-dialog-entry-area-submitsec .reflex-feedback-preloader').show();
-        $("#reflex-feedback-dialog-entry-area button").unbind();
-        $("#reflex-feedback-dialog-entry-area button").button('disable');
-
-        Reflex.sendFeedback(Reflex.feedbackType, $('#reflex-feedback-dialog-entry-area-txt').val());
-
-    });
+    
 }
 
 Reflex.showDialog = function ()
 {
-    $('#reflex-feedback-dialog').dialog({ title: 'Feedback...' }, { width: 520 }, { modal: true }, { resizable: false }, { draggable: true },
+    $('#reflex-feedback-dialog').dialog({ title: 'Feedback...' }, { width: 640 }, { modal: true }, { resizable: false }, { draggable: true },
                                         { beforeClose: function (event, ui)
                                         {
                                             Reflex.txtAreaHasDefault = true;
                                             Reflex.menuIdeaSelect();
                                             $("#reflex-feedback-dialog-entry-area button").unbind();
                                             $('#reflex-feedback-dialog-entry-area-txt').unbind();
-                                            $('#reflex-feedback-dialog-entry-area-submitsec .reflex-feedback-preloader').hide();
+                                            $('#reflex-feedback-dialog-entry-area-submitsec .reflex-feedback-sending-msg').hide();
                                         }
                                         },
 			        					{ open: function (event, ui)
 			        					{
 
 			        					    $('#reflex-feedback-send-failure-msg').hide();
-
-			        					    $("#reflex-feedback-dialog-entry-area button").button('refresh');
-			        					    $("#reflex-feedback-dialog-entry-area button").button('enable');
-			        					    Reflex.bindSendButton();
+			        					    $('#reflex-feedback-dialog-entry-area-submitsec .reflex-feedback-sending-msg').hide();
 
 			        					    $('#reflex-feedback-dialog-entry-area-txt').focusin(function ()
 			        					    {
@@ -138,40 +122,32 @@ Reflex.showDialog = function ()
 			        					        }
 			        					    });
 
-			        					    var footer = '<div style="margin:5px 10px;"><p style="font-size: 10px; color: rgb(136, 136, 136);">Reflex feedback widget<br />&copy; Avishkar Autar<br />Some Icons by <a href="http://p.yusukekamiyamane.com/" style="font-family: inherit; font-size: inherit; color: inherit;">Yusuke Kamiyamane</a>.</p></div>';
+			        					    var footer = '<div style="margin:5px 10px;"><p style="font-size: 10px; color: rgb(136, 136, 136);">Reflex feedback widget<br />&copy; 2010-2011 Avishkar Autar</p></div>';
 			        					    $('.ui-dialog-buttonpane', $('#reflex-feedback-dialog').parent()).append(footer);
+			        					    $('.ui-dialog-buttonpane .ui-button-text', $('#reflex-feedback-dialog').parent()).css('padding', '0.7em 1em');
 
 			        					}
 			        					},
-                                        { buttons: { "Close": function ()
+                                        { buttons: { "Send Feedback": function ()
                                         {
-                                            $('#reflex-feedback-dialog').dialog('close');
+                                            $('#reflex-feedback-dialog').dialog('disable');
+                                            $('#reflex-feedback-dialog-entry-area-submitsec .reflex-feedback-sending-msg').show();
+                                            Reflex.sendFeedback(Reflex.feedbackType, $('#reflex-feedback-dialog-entry-area-txt').val());
                                         }
                                         }
                                         });
 }
 
 Reflex.menuReset = function ()
-{
-    $('#reflex-feedback-dialog-menu .reflex-feedback-icondiv').css('opacity', '1.0');
-    $('#reflex-feedback-dialog-menu .reflex-feedback-icondiv').css('filter', 'alpha(opacity=100)');
-    $('#reflex-feedback-dialog-menu a').css('color', '#888');
-    $('#reflex-feedback-dialog-menu li').css('border', '2px solid transparent');
-}
+{ }
 
 Reflex.menuIdeaSelect = function ()
 {
     Reflex.menuReset();
-
-    $('#reflex-feedback-dialog-menu-idea .reflex-feedback-icondiv').css('opacity', '1.0');
-    $('#reflex-feedback-dialog-menu-idea .reflex-feedback-icondiv').css('filter', 'alpha(opacity=100)');
-    $('#reflex-feedback-dialog-menu-idea a').css('color', '#000');
-    $('#reflex-feedback-dialog-menu-idea').css('border', '2px solid #ddd');
-
     if (Reflex.txtAreaHasDefault) {
         Reflex.curDefaultTxt = 'Tell us about your idea...';
         $('#reflex-feedback-dialog-entry-area-txt').val(Reflex.curDefaultTxt);
-        $('#reflex-feedback-dialog-entry-area-txt').css('color', '#ccc');
+        $('#reflex-feedback-dialog-entry-area-txt').css('color', Reflex.ghostTxtColor);
     }
 
     Reflex.feedbackType = 'idea';
@@ -180,16 +156,10 @@ Reflex.menuIdeaSelect = function ()
 Reflex.menuQuestionSelect = function ()
 {
     Reflex.menuReset();
-
-    $('#reflex-feedback-dialog-menu-question .reflex-feedback-icondiv').css('opacity', '1.0');
-    $('#reflex-feedback-dialog-menu-question .reflex-feedback-icondiv').css('filter', 'alpha(opacity=100)');
-    $('#reflex-feedback-dialog-menu-question a').css('color', '#000');
-    $('#reflex-feedback-dialog-menu-question').css('border', '2px solid #ddd');
-
     if (Reflex.txtAreaHasDefault) {
         Reflex.curDefaultTxt = 'What\'s on your mind...';
         $('#reflex-feedback-dialog-entry-area-txt').val(Reflex.curDefaultTxt);
-        $('#reflex-feedback-dialog-entry-area-txt').css('color', '#ccc');
+        $('#reflex-feedback-dialog-entry-area-txt').css('color', Reflex.ghostTxtColor);
     }
 
     Reflex.feedbackType = 'question';
@@ -198,16 +168,10 @@ Reflex.menuQuestionSelect = function ()
 Reflex.menuProblemSelect = function ()
 {
     Reflex.menuReset();
-
-    $('#reflex-feedback-dialog-menu-problem .reflex-feedback-icondiv').css('opacity', '1.0');
-    $('#reflex-feedback-dialog-menu-problem .reflex-feedback-icondiv').css('filter', 'alpha(opacity=100)');
-    $('#reflex-feedback-dialog-menu-problem a').css('color', '#000');
-    $('#reflex-feedback-dialog-menu-problem').css('border', '2px solid #ddd');
-
     if (Reflex.txtAreaHasDefault) {
         Reflex.curDefaultTxt = 'Tell us about your problem...';
         $('#reflex-feedback-dialog-entry-area-txt').val(Reflex.curDefaultTxt);
-        $('#reflex-feedback-dialog-entry-area-txt').css('color', '#ccc');
+        $('#reflex-feedback-dialog-entry-area-txt').css('color', Reflex.ghostTxtColor);
     }
 
     Reflex.feedbackType = 'problem';
@@ -216,16 +180,10 @@ Reflex.menuProblemSelect = function ()
 Reflex.menuLikeSelect = function ()
 {
     Reflex.menuReset();
-
-    $('#reflex-feedback-dialog-menu-like .reflex-feedback-icondiv').css('opacity', '1.0');
-    $('#reflex-feedback-dialog-menu-like .reflex-feedback-icondiv').css('filter', 'alpha(opacity=100)');
-    $('#reflex-feedback-dialog-menu-like a').css('color', '#000');
-    $('#reflex-feedback-dialog-menu-like').css('border', '2px solid #ddd');
-
     if (Reflex.txtAreaHasDefault) {
         Reflex.curDefaultTxt = 'What do you like...';
         $('#reflex-feedback-dialog-entry-area-txt').val(Reflex.curDefaultTxt);
-        $('#reflex-feedback-dialog-entry-area-txt').css('color', '#ccc');
+        $('#reflex-feedback-dialog-entry-area-txt').css('color', Reflex.ghostTxtColor);
     }
 
     Reflex.feedbackType = 'like';
@@ -236,15 +194,42 @@ Reflex.init = function (body, feedbackPostUrl)
 {
     Reflex.feedbackPostUrl = feedbackPostUrl;
 
-    var css = '<style type="text/css"> #reflex-feedback-dialog-menu { margin:30px 0px 10px 0px; float:left; margin-left:20px; } #reflex-feedback-dialog-menu li { border:1px solid transparent; background:url(reflex.content/btn-bg1.png) 0 0 repeat-x; border-radius:10px; -moz-border-radius:10px; clear:both; margin:10px 0px; height:30px; width:90px; position:relative; display:block; } #reflex-feedback-dialog-menu .reflex-feedback-icondiv { opacity:1.0;filter:alpha(opacity=100); width:16px; height:16px; background:url(reflex.content/icons.png) 0 0 no-repeat; position:absolute; top:-5px; left:-5px; } #reflex-feedback-dialog-menu a { display:block; width:100%; height:100%; font-family:trebuchet MS; font-size:16px; text-indent:15px; color:#888; padding-top:3px; } #reflex-feedback-dialog-entry-area { float:left; width:65%; margin:20px 25px; } #reflex-feedback-dialog-entry-area-txt { width:100%; padding:7px; color:#ccc; font-family:Tahoma; font-size:12px; } #reflex-feedback-dialog-entry-area-submitsec { margin-top:10px; margin-left:100px; } .reflex-feedback-preloader { margin-right:10px; vertical-align:middle; display:none; }</style>';
+    var css = '<style type="text/css"> .reflex-feedback-dialog-menu-icon { display:inline-block; margin-top:-3px; width:16px; height:16px; background-image:url(reflex.content/icons.png); background-repeat:no-repeat; vertical-align:middle; } #reflex-feedback-dialog-menu { margin:30px auto; text-align:center; } #reflex-feedback-dialog-menu .reflex-feedback-icondiv { opacity:1.0;filter:alpha(opacity=100); width:16px; height:16px; background:url(reflex.content/icons.png) 0 0 no-repeat; position:absolute; top:-5px; left:-5px; } #reflex-feedback-dialog-menu a { display:block; width:100%; height:100%; font-family:trebuchet MS; font-size:16px; text-indent:15px; color:#888; padding-top:3px; } #reflex-feedback-dialog-entry-area { width:85%; margin:20px auto; } #reflex-feedback-dialog-entry-area-txt { width:100%; padding:7px; color:#ccc; font-family:Tahoma; font-size:12px; } #reflex-feedback-dialog-entry-area-submitsec .reflex-feedback-sending-msg { text-align:center; display:none; } .reflex-feedback-preloader { margin-right:5px; vertical-align:middle; }</style>';
     $(body).append(css);
 
-    var dialog = '<div style="display:none;" id="reflex-feedback-dialog"><ul id="reflex-feedback-dialog-menu"><li id="reflex-feedback-dialog-menu-idea"><div class="reflex-feedback-icondiv" style="background-position:-32px 0px;"></div><a onclick="Reflex.menuIdeaSelect(); return false;" href="#">Idea</a></li><li id="reflex-feedback-dialog-menu-question"><div class="reflex-feedback-icondiv" style="background-position:-48px 0px;"></div><a onclick="Reflex.menuQuestionSelect(); return false;" href="#">Question</a></li><li id="reflex-feedback-dialog-menu-problem"><div class="reflex-feedback-icondiv" style="background-position:0px 0px;"></div><a onclick="Reflex.menuProblemSelect(); return false;" href="#">Problem</a></li><li id="reflex-feedback-dialog-menu-like"><div class="reflex-feedback-icondiv" style="background-position:-16px 0px;"></div><a onclick="Reflex.menuLikeSelect(); return false;" href="#">Like</a></li></ul><div id="reflex-feedback-dialog-entry-area"><textarea id="reflex-feedback-dialog-entry-area-txt" name="reflex-feedback-dialog-entry-area-txt" rows="12"></textarea><div id="reflex-feedback-dialog-entry-area-submitsec"><img class="reflex-feedback-preloader" src="reflex.content/preloader.gif" alt="preloader" /><button>Send Feedback</button></div><p id="reflex-feedback-send-failure-msg" style="display:none; text-align:center; font-weight:bold; margin-top:20px;">Failed to send your message. Please try again.</p></div></div>';
+    var dialog = '<div style="display:none; font-size:12px;" id="reflex-feedback-dialog"><div id="reflex-feedback-dialog-menu"><input type="radio" id="reflex-feedback-dialog-menu-idea" name="reflex-feedback-dialog-menu-radio" checked="checked" /><label id="reflex-feedback-dialog-menu-idea-lbl" for="reflex-feedback-dialog-menu-idea">Idea</label><input type="radio" id="reflex-feedback-dialog-menu-question" name="reflex-feedback-dialog-menu-radio" /><label id="reflex-feedback-dialog-menu-question-lbl" for="reflex-feedback-dialog-menu-question">Question</label><input type="radio" id="reflex-feedback-dialog-menu-problem" name="reflex-feedback-dialog-menu-radio" /><label id="reflex-feedback-dialog-menu-problem-lbl" for="reflex-feedback-dialog-menu-problem">Problem</label><input type="radio" id="reflex-feedback-dialog-menu-like" name="reflex-feedback-dialog-menu-radio" /><label id="reflex-feedback-dialog-menu-like-lbl" for="reflex-feedback-dialog-menu-like">Like</label></div><div id="reflex-feedback-dialog-entry-area"><textarea id="reflex-feedback-dialog-entry-area-txt" name="reflex-feedback-dialog-entry-area-txt" rows="12"></textarea><div id="reflex-feedback-dialog-entry-area-submitsec"><p class="reflex-feedback-sending-msg"><img class="reflex-feedback-preloader" src="reflex.content/preloader.gif" alt="preloader" />&nbsp;Sending...</p></div><p id="reflex-feedback-send-failure-msg" style="display:none; text-align:center; font-weight:bold; margin-top:20px;">Failed to send your message. Please try again.</p></div></div>';
     var successDialog = '<div style="display:none; text-align:center; padding:30px; font-size:17px; font-weight:bold;" id="reflex-feedback-send-success-dialog"><p>Thank you for your feedback!</p></div>'
     var widgetMarkup = '<div style="position:fixed; top:37%; left:0; background-color:#f00; width:30px; height:100px;" id="reflex-feedback-widget"><a onclick="Reflex.showDialog(); return false;" style="display:block; width:100%; height:100%; background:url(reflex.content/feedback.png) 0 0 no-repeat; text-indent:-999999px;" href="#">FEEDBACK</a>' + dialog + successDialog + '</div>';
 
     $(body).append(widgetMarkup);
     $("#reflex-feedback-dialog-entry-area button").button();
+    $('#reflex-feedback-dialog-menu').buttonset();
+    $('#reflex-feedback-dialog-menu .ui-button-text').prepend("<div class='reflex-feedback-dialog-menu-icon'></div>&nbsp;");
+    $('#reflex-feedback-dialog-menu-idea-lbl .reflex-feedback-dialog-menu-icon').css('background-position', '-32px center');
+    $('#reflex-feedback-dialog-menu-question-lbl .reflex-feedback-dialog-menu-icon').css('background-position', '-48px center');
+    $('#reflex-feedback-dialog-menu-problem-lbl .reflex-feedback-dialog-menu-icon').css('background-position', '0px center');
+    $('#reflex-feedback-dialog-menu-like-lbl .reflex-feedback-dialog-menu-icon').css('background-position', '-16px center');
+    $('#reflex-feedback-dialog-menu .ui-button-text').css('padding', '0.7em 1em');
+
+    $("#reflex-feedback-dialog-menu-idea").click(function ()
+    {
+        if ($(this).is(':checked')) { Reflex.menuIdeaSelect(); }
+    });
+
+    $("#reflex-feedback-dialog-menu-question").click(function ()
+    {
+        if ($(this).is(':checked')) { Reflex.menuQuestionSelect(); }
+    });
+
+    $("#reflex-feedback-dialog-menu-problem").click(function ()
+    {
+        if ($(this).is(':checked')) { Reflex.menuProblemSelect(); }
+    });
+
+    $("#reflex-feedback-dialog-menu-like").click(function ()
+    {
+        if ($(this).is(':checked')) { Reflex.menuLikeSelect(); }
+    });
 
 
     Reflex.menuIdeaSelect();
