19dd3c5ef5a8a2d9a3697b2c7b2eeeefd2716fef
Avishkar Autar <aautar@gmail.com>
2011-04-02 18:44:09 -0400

    v0.3 + Design tweaks (border, box-shadow, border-radius on textarea; padding-top on copyright) + widgetPos argument added to init(), valid values => 'left', 'right'

--- reflex.js
+++ reflex.js
@@ -1,6 +1,6 @@
 /*
     
-Reflex Feedback v0.2
+Reflex Feedback v0.3
 AJAX, JQuery UI widget for user feedback
 
 Copyright (c) 2010-2011, Avishkar Autar
@@ -27,16 +27,26 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+
+CHANGES
+
+v0.3
++ Design tweaks (border, box-shadow, border-radius on textarea; padding-top on copyright)
++ widgetPos argument added to init(), valid values => 'left', 'right'
+
+
 */
+
 
 var Reflex = {}
 
-Reflex.version = 0.2;
+Reflex.version = 0.3;
 Reflex.feedbackType = 'idea';
 Reflex.txtAreaHasDefault = true;
 Reflex.curDefaultTxt = '';
 Reflex.feedbackPostUrl = 'test.php';
 Reflex.ghostTxtColor = '#999';
+Reflex.widgetPos = 'left';
 
 Reflex.sendFeedback = function (feedbackType, feedbackTxt)
 {
@@ -122,7 +132,7 @@ Reflex.showDialog = function ()
 			        					        }
 			        					    });
 
-			        					    var footer = '<div style="margin:5px 10px;"><p style="font-size: 10px; color: rgb(136, 136, 136);">Reflex feedback widget<br />&copy; 2010-2011 Avishkar Autar</p></div>';
+			        					    var footer = '<div style="margin:5px 10px;"><p style="padding-top:5px; font-size: 10px; color: rgb(136, 136, 136);">Reflex feedback widget<br />&copy; 2010-2011 Avishkar Autar</p></div>';
 			        					    $('.ui-dialog-buttonpane', $('#reflex-feedback-dialog').parent()).append(footer);
 			        					    $('.ui-dialog-buttonpane .ui-button-text', $('#reflex-feedback-dialog').parent()).css('padding', '0.7em 1em');
 
@@ -190,16 +200,30 @@ Reflex.menuLikeSelect = function ()
 }
 
 // sendFeedbackFunc([string] feedbackType, [string] feedbackText, [function] onSendSuccess, [function] onSendError);
-Reflex.init = function (body, feedbackPostUrl)
+Reflex.init = function (body, feedbackPostUrl, widgetPos)
 {
     Reflex.feedbackPostUrl = feedbackPostUrl;
 
-    var css = '<style type="text/css"> .reflex-feedback-dialog-menu-icon { display:inline-block; margin-top:-3px; width:16px; height:16px; background-image:url(reflex.content/icons.png); background-repeat:no-repeat; vertical-align:middle; } #reflex-feedback-dialog-menu { margin:30px auto; text-align:center; } #reflex-feedback-dialog-menu .reflex-feedback-icondiv { opacity:1.0;filter:alpha(opacity=100); width:16px; height:16px; background:url(reflex.content/icons.png) 0 0 no-repeat; position:absolute; top:-5px; left:-5px; } #reflex-feedback-dialog-menu a { display:block; width:100%; height:100%; font-family:trebuchet MS; font-size:16px; text-indent:15px; color:#888; padding-top:3px; } #reflex-feedback-dialog-entry-area { width:85%; margin:20px auto; } #reflex-feedback-dialog-entry-area-txt { width:100%; padding:7px; color:#ccc; font-family:Tahoma; font-size:12px; } #reflex-feedback-dialog-entry-area-submitsec .reflex-feedback-sending-msg { text-align:center; display:none; } .reflex-feedback-preloader { margin-right:5px; vertical-align:middle; }</style>';
+    if (widgetPos == 'right') {
+        Reflex.widgetPos = 'right';
+    }
+
+    var css = '<style type="text/css"> .reflex-feedback-dialog-menu-icon { display:inline-block; margin-top:-3px; width:16px; height:16px; background-image:url(reflex.content/icons.png); background-repeat:no-repeat; vertical-align:middle; } #reflex-feedback-dialog-menu { margin:30px auto; text-align:center; } #reflex-feedback-dialog-menu .reflex-feedback-icondiv { opacity:1.0;filter:alpha(opacity=100); width:16px; height:16px; background:url(reflex.content/icons.png) 0 0 no-repeat; position:absolute; top:-5px; left:-5px; } #reflex-feedback-dialog-menu a { display:block; width:100%; height:100%; font-family:trebuchet MS; font-size:16px; text-indent:15px; color:#888; padding-top:3px; } #reflex-feedback-dialog-entry-area { width:85%; margin:20px auto; } #reflex-feedback-dialog-entry-area-txt { border:1px solid #ccc; border-radius:3px; box-shadow:1px 1px 1px #ccc; width:100%; padding:7px; color:#ccc; font-family:Tahoma; font-size:12px; } #reflex-feedback-dialog-entry-area-submitsec .reflex-feedback-sending-msg { text-align:center; display:none; } .reflex-feedback-preloader { margin-right:5px; vertical-align:middle; }</style>';
     $(body).append(css);
 
     var dialog = '<div style="display:none; font-size:12px;" id="reflex-feedback-dialog"><div id="reflex-feedback-dialog-menu"><input type="radio" id="reflex-feedback-dialog-menu-idea" name="reflex-feedback-dialog-menu-radio" checked="checked" /><label id="reflex-feedback-dialog-menu-idea-lbl" for="reflex-feedback-dialog-menu-idea">Idea</label><input type="radio" id="reflex-feedback-dialog-menu-question" name="reflex-feedback-dialog-menu-radio" /><label id="reflex-feedback-dialog-menu-question-lbl" for="reflex-feedback-dialog-menu-question">Question</label><input type="radio" id="reflex-feedback-dialog-menu-problem" name="reflex-feedback-dialog-menu-radio" /><label id="reflex-feedback-dialog-menu-problem-lbl" for="reflex-feedback-dialog-menu-problem">Problem</label><input type="radio" id="reflex-feedback-dialog-menu-like" name="reflex-feedback-dialog-menu-radio" /><label id="reflex-feedback-dialog-menu-like-lbl" for="reflex-feedback-dialog-menu-like">Like</label></div><div id="reflex-feedback-dialog-entry-area"><textarea id="reflex-feedback-dialog-entry-area-txt" name="reflex-feedback-dialog-entry-area-txt" rows="12"></textarea><div id="reflex-feedback-dialog-entry-area-submitsec"><p class="reflex-feedback-sending-msg"><img class="reflex-feedback-preloader" src="reflex.content/preloader.gif" alt="preloader" />&nbsp;Sending...</p></div><p id="reflex-feedback-send-failure-msg" style="display:none; text-align:center; font-weight:bold; margin-top:20px;">Failed to send your message. Please try again.</p></div></div>';
     var successDialog = '<div style="display:none; text-align:center; padding:30px; font-size:17px; font-weight:bold;" id="reflex-feedback-send-success-dialog"><p>Thank you for your feedback!</p></div>'
-    var widgetMarkup = '<div style="position:fixed; top:37%; left:0; background-color:#f00; width:30px; height:100px;" id="reflex-feedback-widget"><a onclick="Reflex.showDialog(); return false;" style="display:block; width:100%; height:100%; background:url(reflex.content/feedback.png) 0 0 no-repeat; text-indent:-999999px;" href="#">FEEDBACK</a>' + dialog + successDialog + '</div>';
+
+
+    var widgetMarkupStyle = '';
+    if (Reflex.widgetPos == 'left') {
+        widgetMarkupStyle = "position:fixed; top:37%; left:0; background-color:#f00; width:30px; height:100px;";
+    }
+    else {
+        widgetMarkupStyle = "position:fixed; top:37%; right:0; background-color:#f00; width:30px; height:100px;";
+    }
+
+    var widgetMarkup = '<div style="' + widgetMarkupStyle + '" id="reflex-feedback-widget"><a onclick="Reflex.showDialog(); return false;" style="display:block; width:100%; height:100%; background:url(reflex.content/feedback.png) 0 0 no-repeat; text-indent:-999999px;" href="#">FEEDBACK</a>' + dialog + successDialog + '</div>';
 
     $(body).append(widgetMarkup);
     $("#reflex-feedback-dialog-entry-area button").button();
